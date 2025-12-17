807af5e01fe90ce5a23a83413678141cb91d791d
field <tianyifan854@gmail.com>
2025-12-17 13:23:15 +0800

    feat: add disk usage report script for linux study

---
+++ Code/disk_usage_report.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#The first shell script written with the help of Gemini and Chatgpt
+echo "----------Disk usage space report-----------"
+df -h
+echo "--------------------------------------------"
